9e42bc36423ab736fc3fb442756df3c5afa268db
dylanwithgravity <dylanwithgravity@gmail.com>
2018-12-02 19:08:43 -0500

    Auto Layout complete

--- Apple Pie/Controllers/ViewController.swift
+++ Apple Pie/Controllers/ViewController.swift
@@ -9,12 +9,38 @@
 import UIKit
 
 class ViewController: UIViewController {
+    
+    var listOfWords = ["pizza", "hotel", "glorious", "incandescent", "bug"]
+    let incorrectMovesAllowed = 7
+    var totalWins = 0
+    var totalLosses = 0
+    var currentGame: Game!
+    
+    @IBOutlet var letterButtons: [UIButton]!
+    @IBOutlet weak var treeImageView: UIImageView!
+    @IBOutlet weak var correctWordLabel: UILabel!
+    @IBOutlet weak var scoreLabel: UILabel!
 
+    
     override func viewDidLoad() {
         super.viewDidLoad()
-        // Do any additional setup after loading the view, typically from a nib.
+        newRound()
+    }
+    
+    func newRound() {
+        let newWord = listOfWords.removeFirst()
+        currentGame = Game(word: newWord, incorrectMovesRemaining: incorrectMovesAllowed)
+        updateUI()
+    }
+    
+    func updateUI() {
+        scoreLabel.text = "Wins: \(totalWins), Losses: \(totalLosses)"
+        treeImageView.image = UIImage(named: "Tree \(currentGame.incorrectMovesRemaining)")
     }
 
-
+    @IBAction func buttonTapped(_ sender: UIButton) {
+        sender.isEnabled = false
+    }
+    
 }
 
---
+++ Apple Pie/Model/Game.swift
@@ -0,0 +1,16 @@
+//
+//  Game.swift
+//  Apple Pie
+//
+//  Created by Dylan Williamson on 12/2/18.
+//  Copyright © 2018 Dylan Williamson. All rights reserved.
+//
+
+import Foundation
+
+
+struct Game {
+    var word: String
+    var incorrectMovesRemaining: Int
+    
+}
